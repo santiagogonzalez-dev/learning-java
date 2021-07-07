d7f3ff5133504d5a8fe743f96c1e3ee37b07e2dc
Santiago Gonzalez <santiagogonzalez@gamil.com>
2021-07-07 17:06:16 -0300

    Introduction to java

---
+++ javaintro/src/main/java/santiagogonzalez/javaintro/E1.java
@@ -0,0 +1,9 @@
+package santiagogonzalez.javaintro;
+
+import java.util.Scanner;
+
+public class E1 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    }
+}
