800a13382d11c58267b5f3bce439e73c1418db4a
Santiago Gonzalez <santiagogonzalez@gamil.com>
2021-07-07 18:47:16 -0300

    Ejercicio 1

--- javaintro/src/main/java/santiagogonzalez/javaintro/E1.java
+++ javaintro/src/main/java/santiagogonzalez/javaintro/E1.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class E1 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        
+        System.out.println("Ingrese primer número");
+        int numEntero = leer.nextInt(); // Leer un entero
+        System.out.println("Ingrese segundo número");
+        int numEnter2 = leer.nextInt(); // Leer segundo entero
+        
+        int resultado = numEntero + numEnter2;
+        System.out.println("La suma de estos dos es de: " + resultado);
+        // csj
     }
 }
