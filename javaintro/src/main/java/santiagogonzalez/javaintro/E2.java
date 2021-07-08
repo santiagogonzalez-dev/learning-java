a0bdf7767cdf75ccde54b4ef04a5d7b3a1b2f6d6
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-07-08 11:10:29 -0300

    fixed names

--- javaintro/src/main/java/santiagogonzalez/javaintro/E2.java
+++ javaintro/src/main/java/santiagogonzalez/javaintro/E2.java
@@ -1,12 +1,18 @@
 package santiagogonzalez.javaintro;
+
 import java.util.Scanner;
+
 public class E2 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         
-        System.out.println("Ingresa tu nombre");
-        String nombre = leer.nextLine();
-        System.out.println("Tu nombre es: " + nombre);
+        System.out.println("Ingrese primer número");
+        int numEntero = leer.nextInt(); // Leer un entero
+        System.out.println("Ingrese segundo número");
+        int numEnter2 = leer.nextInt(); // Leer segundo entero
+        
+        int resultado = numEntero + numEnter2;
+        System.out.println("La suma de estos dos es de: " + resultado);
+        // csj
     }
-    
 }
