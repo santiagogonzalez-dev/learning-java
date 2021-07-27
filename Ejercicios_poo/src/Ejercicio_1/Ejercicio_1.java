9409c1e86cd211a97f643e6886833de66138f17c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-07-27 12:58:56 -0300

    created directory

---
+++ Ejercicios_poo/src/Ejercicio_1/Ejercicio_1.java
@@ -0,0 +1,13 @@
+package Ejercicio_1;
+
+/**
+ *
+ * @author st
+ */
+public class Ejercicio_1 {
+
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
--- intro_oop/src/intro_oop/Ejercicio_1.java
+++
@@ -1,9 +0,0 @@
-package intro_oop;
-
-public class Ejercicio_1 {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
