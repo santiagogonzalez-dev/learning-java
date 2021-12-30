45f9cf130b47847fa72ecaec566be6cd1c9b592c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-12-30 03:16:11 -0300

    hello world in maven working

---
+++ hello-world-maven/src/main/java/com/santigo_zero/app/App.java
@@ -0,0 +1,12 @@
+package com.santigo_zero.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello, World!" );
+        System.out.println( "Bye, World!" );
+    }
+}
