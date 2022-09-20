a28821a3cc048dcb4bb87abd860d2a84f960adfe
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-09-19 22:47:20 -0300

    --all

---
+++ hello-world/src/main/java/com/santigo/app/App.java
@@ -0,0 +1,9 @@
+package com.santigo.app;
+
+// Relearning Java for the second time.
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello, World for the second time!");
+  }
+}
